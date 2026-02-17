6a603c49501b7ed14e0465252c855975bef4a662
aminwhat <m.aminwhat@gmail.com>
2026-02-18 00:39:57 +0330

    add google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const vazir = Vazirmatn({ subsets: ["arabic", "latin"], display: "swap" });
@@ -92,9 +93,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const gtmId = "GTM-55QHDQ6G";
+
   return (
     <html lang="fa" dir="rtl">
       <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+        </Script>
+
         {/* Structured Data for SEO (JSON-LD) */}
         <script
           type="application/ld+json"
@@ -143,6 +154,15 @@ export default function RootLayout({
         className={`${vazir.className} bg-black text-white antialiased overflow-x-hidden`}
       >
         {children}
+
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
