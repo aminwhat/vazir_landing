1883bcf4a461d9ab710a0a36f0cd5aae0431508c
aminwhat <m.aminwhat@gmail.com>
2026-02-18 00:47:32 +0330

    google analytics tag manager added

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,17 +93,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const gtmId = "GTM-55QHDQ6G";
+  const gaId = "G-X910ST8C4E";
 
   return (
     <html lang="fa" dir="rtl">
       <head>
-        <Script id="gtm" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${gtmId}');`}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${gaId}');`}
         </Script>
 
         {/* Structured Data for SEO (JSON-LD) */}
@@ -154,15 +157,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         className={`${vazir.className} bg-black text-white antialiased overflow-x-hidden`}
       >
         {children}
-
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
       </body>
     </html>
   );
